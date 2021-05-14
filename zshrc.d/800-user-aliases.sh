a95c3bdd37c0fed2a9f7fae9a344d2670ba0b5b1
bmatsuo-at-luthersystems <bryan.matsuo@luthersystems.com>
2021-05-13 19:41:51 -0700

    More zsh aliases

---
+++ zshrc.d/800-user-aliases.sh
@@ -0,0 +1,3 @@
+alias cloud-ssh="ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+alias cloud-scp="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+alias vim="nvim"
